448da60670b18112383460a4e2cd5642b7177573
birchpress <birchpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 13:12:22 +0000

    new version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/birchschedule/tags/1.2.1@633981 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/common.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+    window.namespace = function(){
+        var len1 = arguments.length,
+            i = 0,
+            len2,
+            j,
+            main,
+            ns,
+            sub,
+            current;
+
+        for(; i < len1; ++i) {
+            main = arguments[i];
+            ns = arguments[i].split('.');
+            current = window[ns[0]];
+            if (current === undefined) {
+                current = window[ns[0]] = {};
+            }
+            sub = ns.slice(1);
+            len2 = sub.length;
+            for(j = 0; j < len2; ++j) {
+                current = current[sub[j]] = current[sub[j]] || {};
+            }
+        }
+        return current;
+    }
+    namespace('birchschedule');
+
+    birchschedule.changeStateUi = function(countryId, stateId, provinceId){
+        var countryEl = $('#' + countryId);
+        var stateEl = $('#' + stateId);
+        var provinceEl = $('#' + provinceId);
+        var country = countryEl.val();
+        if(country == 'US'){
+            stateEl.show();
+            provinceEl.hide();
+        } else {
+            stateEl.hide();
+            provinceEl.show();
+        }
+    }
+    
+    birchschedule.changeStaffOptions = function(serviceStaffMap, locationStaffMap) {
+        var staffId = $('#birs_appointment_staff').val();
+
+        var availableStaff = _.pick(serviceStaffMap, _.keys(locationStaffMap));
+        var html = "";
+        $.each(availableStaff, function(key, value){
+            html += '<option value="' + key + '">' + value + '</option>';                            
+        });
+        $('#birs_appointment_staff').html(html);
+        $('#birs_appointment_staff').val(staffId);
+    }
+        
+
+});
---
+++ classes/birs-help-view.php
@@ -0,0 +1,80 @@
+<?php
+
+class BIRS_Help_View extends BIRS_Admin_View {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    function admin_init() {
+        parent::admin_init();
+        add_meta_box('birs_help_general', __('Help and Support', 'birchschedule'), array($this, 'render_help_general'), $this->get_page_hook_suffix(), 'main', 'default');
+        add_meta_box('birs_help_version', __('Versions', 'birchschedule'), array($this, 'render_help_version'), $this->get_page_hook_suffix(), 'main', 'default');
+    }
+
+    function get_page_hook_suffix() {
+        global $birchschedule;
+        $help_view = $birchschedule->help_view;
+        return $help_view->page_hook;
+    }
+
+    function render_help_version() {
+        global $birchschedule, $wp_version;
+        $version = $birchschedule->product_version;
+        $product_name = $birchschedule->product_name;
+        ?>
+        <div class="wrap">
+            <table class="form-table">
+                <tbody>
+                    <tr>
+                        <th><?php echo "$product_name"; ?></th>
+                        <td><?php echo "$version" ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "WordPress"; ?></th>
+                        <td><?php echo "$wp_version" ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "PHP"; ?></th>
+                        <td><?php echo phpversion(); ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "MySQL"; ?></th>
+                        <td><?php echo mysql_get_server_info(); ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <?php
+    }
+
+    function render_help_general() {
+        ?>
+        <div class="padding">
+            <p>If you have any questions, please review the <a href="http://www.birchpress.com/support/quick-start-guide">tutorial</a>. If you cannot find the answer you need, please start a <a href="http://www.birchpress.com/support/forums">support topic</a> and we will answer your question as soon as possible.</p>
+            <p>If you find our product helpful, please <a href="http://wordpress.org/extend/plugins/birchschedule">rate it!</a></p>
+            <p>If you find a bug in this plugin or have a suggestion, please submit it through our <a href="http://www.birchpress.com/support/forums">support forums</a> with a clear description.</p>
+        </div>
+        <?php
+    }
+
+    function render_admin_page() {
+        ?>
+        <div id="birchschedule_email_notification" class="wrap">
+            <form method="post" action="">
+                <?php wp_nonce_field('meta-box-order', 'meta-box-order-nonce', false); ?>
+                <div id="poststuff">
+                    <div id="post-body" class="metabox-holder columns-1">
+                        <div id="postbox-container-1" class="postbox-container">
+                            <?php do_meta_boxes($this->get_page_hook_suffix(), 'main', array()) ?>
+                        </div>
+                    </div>
+                    <br class="clear" />
+                </div>
+            </form>
+        </div>
+        <?php
+    }
+
+}
+?>
