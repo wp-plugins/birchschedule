99adac34f2ce6dadbf5bb6109cd1ab3233e50ce1
birchpress <birchpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-23 15:02:58 +0000

    version 1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/birchschedule/tags/1.3.2@643662 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/model/birs-location.php
@@ -0,0 +1,37 @@
+<?php
+
+class BIRS_Location extends BIRS_Model {
+
+    public function __construct($id, $options) {
+        parent::__construct($id, $options);
+        $this['post_type'] = 'birs_location';
+    }
+
+    public function get_assigned_staff() {
+        $query = new BIRS_Model_Query(
+                        array(
+                            'post_type' => 'birs_staff'
+                        ),
+                        array(
+                            'meta_keys' => array(
+                                '_birs_staff_schedule'
+                            ),
+                            'base_keys' => array(
+                                'post_title'
+                            )
+                        )
+        );
+        $assigned_staff = array();
+        $staff = $query->query();
+        foreach ($staff as $the_staff) {
+            $schedule = $the_staff->get_all_schedule($this->ID);
+            if (sizeof($schedule) > 0) {
+                $assigned_staff[$the_staff->ID] = $the_staff->post_title;
+            }
+        }
+        return $assigned_staff;
+    }
+
+}
+
+?>
