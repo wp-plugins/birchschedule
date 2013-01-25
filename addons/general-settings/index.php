14db8f7e5e5cd84f6b704e6aa155c12f577ea903
birchpress <birchpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-25 02:06:43 +0000

    version 1.3.5
    git-svn-id: https://plugins.svn.wordpress.org/birchschedule/tags/1.3.5@658431 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ addons/general-settings/index.php
@@ -0,0 +1,182 @@
+<?php
+
+class BIRS_Addon_Settings_General extends BIRS_Addon {
+    
+    var $default_options;
+    var $update_info = array();
+
+    function __construct() {
+        parent::__construct();
+        add_action('admin_init', array($this, 'init'));
+        add_filter('birchschedule_settings_tabs', array(&$this, 'add_tab'));
+        add_filter('birchschedule_price_label', array($this, 'add_currency_symbol'));
+        add_filter('birchschedule_price', array($this, 'add_currency_symbol'));
+        add_filter('birchschedule_default_calendar_view', array($this, 'get_option_default_calendar_view'));
+        add_action('birchschedule_show_update_notice', array($this, 'show_update_notice'));
+        add_filter('site_transient_update_plugins', array($this, 'get_update_info'), 20);
+        $this->init_constants();
+    }
+    
+    function init_constants() {
+        $this->default_options = array(
+            'currency' => 'USD',
+            'default_calendar_view' => 'agendaWeek'
+        );
+    }
+    
+    function get_addon_dir_name() {
+        return 'general-settings';
+    }
+
+    function init() {
+        register_setting('birchschedule_options', 'birchschedule_options', array($this, 'sanitize_input'));
+        add_settings_section('birchschedule_general', __('General Options', 'birchschedule'), array(&$this, 'render_section_general'), 'birchschedule_settings');
+        do_action('birchschedule_add_settings_section_general_after');
+        add_settings_field('birchschedule_timezone', __('Timezone'), array(&$this, 'render_timezone'), 'birchschedule_settings', 'birchschedule_general');
+        add_settings_field('birchschedule_date_time_format', __('Date Format, Time Format'), array(&$this, 'render_date_time_format'), 'birchschedule_settings', 'birchschedule_general');
+        add_settings_field('birchschedule_currency', __('Currency'), array(&$this, 'render_currency'), 'birchschedule_settings', 'birchschedule_general');
+        add_settings_field('birchschedule_default_calendar_view', __('Default Calendar View'), array(&$this, 'render_default_calendar_view'), 'birchschedule_settings', 'birchschedule_general');
+    }
+
+    function add_tab($tabs) {
+        $tabs['general'] = array(
+            'title' => __('General', 'birchschedule'),
+            'action' => array(&$this, 'render_page'),
+            'order' => 0
+        );
+
+        return $tabs;
+    }
+
+    function add_currency_symbol($arg) {
+        $currencies = $this->util->get_currencies();
+        $currency = $this->get_option_currency();
+        $currency = $currencies[$currency];
+        $symbol = $currency['symbol_right'];
+        if ($symbol == '') {
+            $symbol = $currency['symbol_left'];
+        }
+        if (is_numeric($arg)) {
+            if ($currency['symbol_right']) {
+                $arg .= $symbol;
+            } else {
+                $arg = $symbol . $arg;
+            }
+        } else {
+            $arg = $arg . ' (' . $symbol . ')';
+        }
+
+        return $arg;
+    }
+
+    function get_option_currency($currency = 'USD') {
+        $options = get_option('birchschedule_options');
+        if (isset($options['currency'])) {
+            $currency = $options['currency'];
+        }
+        return $currency;
+    }
+
+    function get_option_default_calendar_view($view = 'agendaWeek') {
+        $options = get_option('birchschedule_options');
+        if (isset($options['default_calendar_view'])) {
+            $view = $options['default_calendar_view'];
+        }
+        return $view;
+    }
+
+    function render_section_general() {
+        echo '';
+    }
+    
+    function get_options() {
+        $options = get_option('birchschedule_options');
+        if($options === false) {
+            add_option('birchschedule_options', $this->default_options);
+            return $this->default_options;
+        }
+        return $options;
+    }
+
+    function render_timezone() {
+        $timezone_url = admin_url('options-general.php');
+        echo "<label>Timezone settings are located <a href='$timezone_url'>here</a>.</label>";
+    }
+    
+    function render_date_time_format() {
+        $timezone_url = admin_url('options-general.php');
+        echo "<label>Date format, time format settings are located <a href='$timezone_url'>here</a>.</label>";
+    }
+    
+    function map_currencies($currency) {
+        if ($currency['symbol_right'] != '') {
+            return $currency['title'] . ' (' . $currency['symbol_right'] . ')';
+        } else {
+            return $currency['title'] . ' (' . $currency['symbol_left'] . ')';
+        }
+    }
+
+    function render_currency() {
+        $currencies = $this->util->get_currencies();
+        $currencies = array_map(array(&$this, 'map_currencies'), $currencies);
+        $currency = $this->get_option_currency();
+        echo '<select id="birchschedule_currency" name="birchschedule_options[currency]">';
+        $this->util->render_html_options($currencies, $currency);
+        echo '</select>';
+    }
+
+    function render_default_calendar_view() {
+        $views = $this->util->get_calendar_views();
+        $default_view = $this->get_option_default_calendar_view();
+        echo '<select id="birchschedule_default_calenar_view" name="birchschedule_options[default_calendar_view]">';
+        $this->util->render_html_options($views, $default_view);
+        echo '</select>';
+    }
+
+    function render_page() {
+        settings_errors();
+        ?>
+        <form action="options.php" method="post">
+            <?php settings_fields('birchschedule_options'); ?>
+            <?php do_settings_sections('birchschedule_settings'); ?>
+            <p class="submit">
+                <input name="Submit" type="submit" class="button-primary"
+                       value="<?php _e('Save changes', 'birchschedule'); ?>" />
+            </p>
+        </form>
+        <?php
+    }
+
+    function sanitize_input($input) {
+        return apply_filters('birchschedule_sanitize_options_input', $input);
+    }
+    
+    function get_update_info($checked_data) {
+        $plugin_slug = "birchschedule";
+        $slug_str = $plugin_slug . '/' . $plugin_slug . '.php';
+        if(isset($checked_data->response[$slug_str])) {
+            $this->update_info = array(
+                'version' => $checked_data->response[$slug_str]->version
+            );
+        }
+        return $checked_data;
+    }
+    
+    function show_update_notice() {
+        global $birchschedule;
+        $product_name = $birchschedule->product_name;
+        $update_url = admin_url('update-core.php');
+        $update_text = "%s %s is available! <a href='$update_url'>Please update now</a>.";
+        if($this->update_info):
+    ?>
+        <div class="updated inline">
+            <p><?php echo sprintf($update_text, $product_name, $this->update_info['version']); ?></p>
+        </div>
+        <?php
+        endif;
+    }
+
+}
+
+$birchschedule->addons['general_settings'] = new BIRS_Addon_Settings_General();
+?>
---
+++ classes/model/birs-staff.php
@@ -0,0 +1,147 @@
+<?php
+
+class BIRS_Staff extends BIRS_Model {
+
+    public function __construct($id, $options) {
+        parent::__construct($id, $options);
+        $this['post_type'] = 'birs_staff';
+    }
+
+    public function get_assigned_service_ids() {
+        $assigned_services = $this['_birs_assigned_services'];
+        $assigned_services = unserialize($assigned_services);
+        $assigned_services = $assigned_services ? $assigned_services : array();
+        return $assigned_services;
+    }
+
+    public function get_all_schedule($location_id = false) {
+        $schedule = $this['_birs_staff_schedule'];
+        if (!isset($schedule)) {
+            $schedule = array();
+        } else {
+            $schedule = unserialize($schedule);
+        }
+        $schedule = $schedule ? $schedule : array();
+        $staff_schedule = array();
+        if ($location_id) {
+            if (isset($schedule[$location_id])) {
+                $staff_schedule = $schedule[$location_id];
+            }
+        } else {
+            $staff_schedule = $schedule;
+        }
+        return $staff_schedule;
+    }
+
+    public function get_avaliable_time($location_id, $service_id, $date) {
+        $util = BIRS_Util::get_instance();
+        $staff_id = $this['ID'];
+        $date = $util->get_wp_datetime(
+                array(
+                    'date' => $date,
+                    'time' => 0
+                ));
+
+        //filtered by predefined schedule.
+        $time_options = $util->get_time_options();
+        $wday = $date->format('w');
+        $staff_schedule = $this->get_all_schedule($location_id);
+        if (isset($staff_schedule[$wday])) {
+            $staff_schedule = $staff_schedule[$wday];
+        } else {
+            return array();
+        }
+        $time_start = $staff_schedule['minutes_start'];
+        $time_end = $staff_schedule['minutes_end'];
+        $temp_time_options = array();
+        foreach ($time_options as $key => $value) {
+            if ($key >= $time_start && $key <= $time_end) {
+                $temp_time_options[$key] = array(
+                    'text' => $value,
+                    'avaliable' => true);
+            }
+        }
+        $time_options = $temp_time_options;
+
+        $service = new BIRS_Service($service_id, array(
+                    'meta_keys' => array(
+                        '_birs_service_length', '_birs_service_length_type',
+                        '_birs_service_padding', '_birs_service_padding_type'
+                    )
+                ));
+        $service->load();
+        $service_len = $service->get_service_length() + $service->get_padding_after();
+        $service_padding_before = $service->get_padding_before();
+
+        //filtered by appointments
+        $timestamp = $date->format('U');
+        $query = new BIRS_Model_Query(
+                        array(
+                            'post_type' => 'birs_appointment',
+                            'meta_query' => array(
+                                array(
+                                    'key' => '_birs_appointment_timestamp',
+                                    'value' => array($timestamp, $timestamp + 3600 * 24),
+                                    'type' => 'numeric',
+                                    'compare' => 'BETWEEN'
+                                ),
+                                array(
+                                    'key' => '_birs_appointment_staff',
+                                    'value' => $staff_id
+                                )
+                            )
+                        ),
+                        array(
+                            'meta_keys' => array(
+                                '_birs_appointment_timestamp', '_birs_appointment_duration', '_birs_appointment_service', '_birs_appointment_padding_before', '_birs_appointment_padding_after'
+                            )
+                        )
+        );
+        $appointments = $query->query();
+        $appointments_time = array();
+        foreach (array_values($appointments) as $appointment) {
+            $busy_time = $appointment['_birs_appointment_timestamp'];
+            $datetime = $util->get_wp_datetime($busy_time);
+            $busy_time = $util->get_day_minutes($datetime) - $appointment['_birs_appointment_padding_before'];
+            $appointment_duration = $appointment['_birs_appointment_duration'] + $appointment['_birs_appointment_padding_before'] + $appointment['_birs_appointment_padding_after'];
+            $appointments_time[] = array(
+                'busy_time' => $busy_time,
+                'duration' => $appointment_duration
+            );
+        }
+        $busy_time = end(array_keys($time_options));
+        $appointments_time[] = array(
+            'busy_time' => $busy_time,
+            'duration' => 0
+        );
+        $busy_time = reset(array_keys($time_options));
+        array_unshift($appointments_time, array(
+            'busy_time' => $busy_time + $service_padding_before,
+            'duration' => 0
+        ));
+        $appointments_time = apply_filters('birchschedule_staff_busy_time', 
+            $appointments_time, $this->ID, $location_id, $date);
+        foreach ($appointments_time as $time) {
+            $busy_time = $time['busy_time'];
+            $appointment_duration = $time['duration'];
+            foreach ($time_options as $key => $value) {
+                if ($key > $busy_time - $service_len - $service_padding_before && $key < $busy_time + $appointment_duration + $service_padding_before) {
+                    $time_options[$key]['avaliable'] = false;
+                }
+            }
+        }
+        $time_options[end(array_keys($time_options))]['avaliable'] = false;
+        return $time_options;
+    }
+
+    public function pre_save() {
+        
+    }
+    
+    public function post_save() {
+        
+    }
+
+}
+
+?>
