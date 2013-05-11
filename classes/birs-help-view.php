ab8041571d15c5499edaae78f340f52899195318
birchpress <birchpress@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-11 14:14:52 +0000

    new version 1.5.5 * Enhancement: Optimize the booking form for responsive themes. * Enhancement: Support French. * Enhancement: Hide the service field in the booking form when there is only one option. * Enhancement: Hide the service provider field in the booking form when there is only one option.
    git-svn-id: https://plugins.svn.wordpress.org/birchschedule/tags/1.5.5@711379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/birs-help-view.php
@@ -0,0 +1,95 @@
+<?php
+
+class BIRS_Help_View extends BIRS_Admin_View {
+
+    function __construct() {
+        parent::__construct();
+        add_filter('birchschedule_get_wp_screen', array($this, 'get_screen'), 10, 2);
+    }
+
+    function admin_init() {
+        parent::admin_init();
+        $screen = apply_filters('birchschedule_get_wp_screen', 
+            null, $this->get_page_hook_suffix());
+        add_meta_box('birs_help_general', __('Help and Support', 'birchschedule'), 
+            array($this, 'render_help_general'), 
+            $screen, 'main', 'default');
+        add_meta_box('birs_help_version', __('Versions', 'birchschedule'), 
+            array($this, 'render_help_version'), 
+            $screen, 'main', 'default');
+    }
+
+    function get_page_hook_suffix() {
+        return "birchschedule_page_help";
+    }
+
+    function get_screen($screen, $hook_name) {
+        $page_hook = $hook_name . '.php';
+        $screen = WP_Screen::get($page_hook);
+        return $screen;
+    }
+
+    function render_help_version() {
+        global $birchschedule, $wp_version;
+        $version = $birchschedule->product_version;
+        $product_name = $birchschedule->product_name;
+        ?>
+        <div class="wrap">
+            <table class="form-table">
+                <tbody>
+                    <tr>
+                        <th><?php echo "$product_name"; ?></th>
+                        <td><?php echo "$version" ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "WordPress"; ?></th>
+                        <td><?php echo "$wp_version" ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "PHP"; ?></th>
+                        <td><?php echo phpversion(); ?></td>
+                    </tr>
+                    <tr>
+                        <th><?php echo "MySQL"; ?></th>
+                        <td><?php echo mysql_get_server_info(); ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <?php
+    }
+
+    function render_help_general() {
+        ?>
+        <div class="padding">
+            <p>If you have any questions, please review the <a href="http://www.birchpress.com/support/quick-start-guide">tutorial</a> first.</p>
+            <p>If you are a <a href="http://www.birchpress.com/products/scheduler-pro/">BirchPress Scheduler Pro</a> user, please submit your question <a href="http://www.birchpress.com/support/submit-a-ticket/">here</a>.</p>
+            <p>If you are a BirchPress Scheduler user, please submit your question through our <a href="http://www.birchpress.com/support/forums">support forum</a>.</p>
+            <p>If you find our product helpful, please <a href="http://wordpress.org/extend/plugins/birchschedule">rate it!</a></p>
+        </div>
+        <?php
+    }
+
+    function render_admin_page() {
+        $screen = apply_filters('birchschedule_get_wp_screen', 
+            null, $this->get_page_hook_suffix());
+        do_action('birchschedule_show_update_notice');
+        ?>
+        <div id="birchschedule_email_notification" class="wrap">
+            <form method="post" action="">
+                <?php wp_nonce_field('meta-box-order', 'meta-box-order-nonce', false); ?>
+                <div id="poststuff">
+                    <div id="post-body" class="metabox-holder columns-1">
+                        <div id="postbox-container-1" class="postbox-container">
+                            <?php do_meta_boxes($screen, 'main', array()) ?>
+                        </div>
+                    </div>
+                    <br class="clear" />
+                </div>
+            </form>
+        </div>
+        <?php
+    }
+
+}
+?>
---
+++ classes/model/birs-appointment.php
@@ -0,0 +1,96 @@
+<?php
+
+class BIRS_Appointment extends BIRS_Model {
+    
+    public function __construct($id, $options=array()) {
+        parent::__construct($id, $options);
+        if(!in_array('post_status', $this->base_keys)) {
+            $this->base_keys[] = 'post_status';
+        }
+        $this['post_type'] = 'birs_appointment';
+    }
+
+    public function pre_save() {
+        $service = new BIRS_Service($this['_birs_appointment_service'], array(
+                    'meta_keys' => array(
+                        '_birs_service_length', '_birs_service_length_type',
+                        '_birs_service_padding', '_birs_service_padding_type'
+                    ),
+                    'base_keys' => array(
+                        'post_title'
+                    )
+                ));
+        $service->load();
+        $this['_birs_appointment_padding_before'] = $service->get_padding_before();
+        $this['_birs_appointment_padding_after'] = $service->get_padding_after();
+        $this['_birs_appointment_duration'] = (int) $this['_birs_appointment_duration'];
+        $client = new BIRS_Client($this['_birs_appointment_client'], array(
+                    'base_keys' => array(
+                        'post_title'
+                    )
+                ));
+        $client->load();
+        $this['post_title'] = $service['post_title'] . ' - ' . $client['post_title'];
+        $this['_birs_appointment_price'] = floatval($this['_birs_appointment_price']);
+        parent::pre_save();
+    }
+
+    public function get_appointment_pre_payment_fee() {
+        $service = new BIRS_Service($this['_birs_appointment_service'],
+            array(
+                'meta_keys' => array(
+                    '_birs_service_pre_payment_fee'
+                )
+            )
+        );
+        $service->load();
+        $service_pre_payment_fee = unserialize($service['_birs_service_pre_payment_fee']);
+        if($service_pre_payment_fee) {
+            if($service_pre_payment_fee['pre_payment_type'] == 'fixed') {
+                return floatval($service_pre_payment_fee['fixed']);
+            }
+            else if($service_pre_payment_fee['pre_payment_type'] == 'percent') {
+                return $service_pre_payment_fee['percent'] * 
+                    $this['_birs_appointment_price'] * 0.01;
+            } else {
+                return 0;
+            }
+        } else {
+            return 0;
+        }
+    }
+
+    public function get_appointment_payments() {
+        $query = new BIRS_Model_Query(
+            array(
+                'post_type' => 'birs_payment',
+                'meta_query' => array(
+                    array(
+                        'key' => '_birs_payment_appointment',
+                        'value' => $this->ID
+                    )
+                )
+            ),
+            array(
+                'meta_keys' => array(
+                    '_birs_payement_appointment', '_birs_payment_client',
+                    '_birs_payment_amount', '_birs_payment_type',
+                    '_birs_payment_trid', '_birs_payment_notes',
+                    '_birs_payment_timestamp', '_birs_payment_currency'
+                )
+            )
+        );
+        $payments = $query->query();
+        return $payments;
+    }
+
+    public function copyFromRequest($request) {
+        parent::copyFromRequest($request);
+        if(isset($request['birs_appointment_status'])) {
+            $this['post_status'] = $request['birs_appointment_status'];
+        }
+    }
+    
+}
+
+?>
